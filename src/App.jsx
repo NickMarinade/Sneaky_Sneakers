23586a92473b1adf1376dce36d79024512b3f4b4
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-05-09 12:16:22 +0200

    searchbar + card styiling on hover

--- src/App.jsx
+++ src/App.jsx
@@ -23,25 +23,113 @@ function App() {
       </header>
 
       <div className="content">
+        <div className="contentHeader">
         <h1>All Sneakers</h1>
+        <div className="search-block">
+          <img src="/img/search.svg" alt="Search" />
+          <input placeholder="Search..."/>
+        </div>
+        </div>
 
-        <div className="card">
-          <img
-            width={153}
-            height={132}
-            src="/img/sneakers/Jordan_Stay_Loyal2.png"
-            alt="Sneakers"
-          />
-          <h5>Jordan Stay Loyal 2 'Black/White/Red'</h5>
-          <div className="cardBottom">
-            <div className="cardPrice">
-              <span>Price:</span>
-              <b>60&#8364;</b>
+        <div className="sneakers">
+          <div className="card">
+            <div className="favorite">
+            <img src="/img/unliked.svg" alt="Unliked" />
             </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
+            <img
+              width={153}
+              height={132}
+              src="/img/sneakers/Jordan_Stay_Loyal2.png"
+              alt="Sneakers"
+            />
+            <h5>Jordan Stay Loyal 2 'Black/White/Red'</h5>
+            <div className="cardBottom">
+              <div className="cardPrice">
+                <span>Price:</span>
+                <b>60&#8364;</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
           </div>
+
+          <div className="card">
+            <img
+              width={153}
+              height={132}
+              src="/img/sneakers/Kyrie_Infinity_Rattan.jpg"
+              alt="Sneakers"
+            />
+            <h5>Kyrie Infinity <br /> 'Rattan'</h5>
+            <div className="cardBottom">
+              <div className="cardPrice">
+                <span>Price:</span>
+                <b>120&#8364;</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={153}
+              height={132}
+              src="/img/sneakers/Peak_Lou_Williams_3_CherryBlossom.png"
+              alt="Sneakers"
+            />
+            <h5>Peak Lou Williams 3 'Cherry Blossom'</h5>
+            <div className="cardBottom">
+              <div className="cardPrice">
+                <span>Price:</span>
+                <b>80&#8364;</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={153}
+              height={132}
+              src="/img/sneakers/Tatum1_Zoo.png"
+              alt="Sneakers"
+            />
+            <h5>Tatum 1 <br /> 'Zoo'</h5>
+            <div className="cardBottom">
+              <div className="cardPrice">
+                <span>Price:</span>
+                <b>90&#8364;</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+
+          {/* <div className="card">
+            <img
+              width={153}
+              height={132}
+              src="/img/sneakers/Zion2_Red_Black.jpg"
+              alt="Sneakers"
+            />
+            <h5>Zion 2 'Red/Black'</h5>
+            <div className="cardBottom">
+              <div className="cardPrice">
+                <span>Price:</span>
+                <b>110&#8364;</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div> */}
+          
         </div>
       </div>
     </div>
